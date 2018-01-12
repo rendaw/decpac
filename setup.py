83ec640a8a8f9c8413cb436e628f40160fdb9b4f
rendaw <spoo@zarbosoft.com>
2018-01-12 12:13:41 +0900

    Fill out setup

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(
     name='decpac',
     version='0.0.1',
     author='rendaw',
+    url='https://github.com/rendaw/decpac',
+    download_url='https://github.com/rendaw/decpac/tarball/v0.0.1',
     license='MIT',
+    description='Arch Linux declarative package management',
+    long_description=open('readme.md', 'r').read(),
+    classifiers=[],
     py_modules=['decpac'],
     install_requires=[
         'luxem==0.0.2',
