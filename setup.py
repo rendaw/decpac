c9b828791ced1f1c13371ac729ed76b5be97fdab
rendaw <>
2020-07-08 22:28:48 +0900

    Update for pypi standards

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup
 
-GEN_version = '0.0.7'
-READ_name = 'decpac'
+GEN_version = "0.0.7"
+READ_name = "decpac"
 
 setup(
     name=READ_name,
     version=GEN_version,
-    author='rendaw',
-    url='https://gitlab.com/rendaw/decpac',
-    download_url='https://gitlab.com/rendaw/decpac/-/archive/v{v}/decpac-v{v}.tar.gz'.format(
-        v=GEN_version),
-    license='MIT',
-    description='Arch Linux declarative package management',
-    long_description=open('readme.md', 'r').read(),
+    author="rendaw",
+    url="https://gitlab.com/rendaw/decpac",
+    download_url="https://gitlab.com/rendaw/decpac/-/archive/v{v}/decpac-v{v}.tar.gz".format(
+        v=GEN_version
+    ),
+    license="MIT",
+    description="Arch Linux declarative package management",
+    long_description=open("readme.md", "r").read(),
+    long_description_content_type="text/markdown",
     classifiers=[],
-    py_modules=['decpac'],
-    install_requires=[
-        'luxem==0.0.2',
-    ],
-    entry_points={
-        'console_scripts': [
-            'decpac=decpac:main',
-        ],
-    },
+    py_modules=["decpac"],
+    install_requires=["luxem==0.0.2"],
+    entry_points={"console_scripts": ["decpac=decpac:main"]},
 )
